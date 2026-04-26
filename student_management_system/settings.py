8dbfa24e66154309ff4d6b2f0bc3fa95ea163361
Buntie <buntie@MacBook-Air.local>
2026-04-26 12:20:23 +0530

    Fix: database configuration fallback to SQLite when DATABASE_URL is empty

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -105,11 +105,14 @@ WSGI_APPLICATION = 'student_management_system.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL', f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}"),
-        conn_max_age=600,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
+if os.getenv('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
